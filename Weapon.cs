7b35b329631e2edbb94d34e18ebe98bc0c36257f
SERGEY <96051914+Sergeiphantom86@users.noreply.github.com>
2025-03-22 00:41:17 +0300

    Update Weapon.cs

--- Weapon.cs
+++ Weapon.cs
@@ -5,12 +5,24 @@ public class Weapon
 
     public Weapon(int damage, int initialBullets)
     {
+        if (damage <= 0)
+            throw new ArgumentException($"Урон не должен быть отрицательным. {damage.ToString()} = {damage}");
+
+        if (initialBullets < 0)
+            throw new ArgumentException($"Количество пуль не может быть отрицательным.{initialBullets.ToString()} = {initialBullets}");
+
         _damage = damage;
-        _bullets = initialBullets >= 0 ? initialBullets : 0;
+        _bullets = initialBullets;
     }
 
     public void Fire(Player player)
     {
+        if (_bullets <= 0)
+            throw new InvalidOperationException($"Патронов не осталось.{_bullets.ToString()} = {_bullets}");
+
+        if (player == null)
+            throw new ArgumentNullException(nameof(player), "Имеет значение null.");
+
         player.ApplyDamage(_damage);
         _bullets--;
     }
@@ -20,17 +32,21 @@ public class Player
 {
     private int _health;
 
-    public Player(int initialHealth)
-    {
-        _health = initialHealth > 0 ? initialHealth : 0;
-    }
-
     public int MaxHealth => _health;
     public bool IsAlive => _health > 0;
 
+    public Player(int health)
+    {
+        if (health <= 0)
+            throw new ArgumentException($"Здоровье не должно быть отрицательным. {health.ToString()} = {health}");
+
+        _health = health;
+    }
+
     public void ApplyDamage(int damage)
     {
-        if (damage < 0) return;
+        if (damage < 0)
+            throw new ArgumentException($"Урон не может быть отрицательным. {damage.ToString()} = {damage}");
 
         _health = Math.Max(_health - damage, 0);
     }
@@ -38,15 +54,18 @@ public class Player
 
 public class Bot
 {
-    private Weapon _weapon;
+    private readonly Weapon _weapon;
 
     public Bot(Weapon weapon)
     {
-        _weapon = weapon;
+        _weapon = weapon ?? throw new ArgumentNullException(nameof(weapon), "Имеет значение null.");
     }
 
     public void OnSeePlayer(Player player)
     {
+        if (player == null)
+            throw new ArgumentNullException(nameof(player), "Имеет значение null.");
+
         if (player.IsAlive)
         {
             _weapon.Fire(player);
